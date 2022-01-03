5cb5df6cf9dfcc9bf2989660d0a571ddf17d5576
Robert Turturica <33758807+RabbitHols@users.noreply.github.com>
2022-01-04 00:31:23 +0100

    Add files via upload

---
+++ precache-manifest.65be25a93abbe77f08340b65abe19014.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "284929a85a35169464fdf4046bffcaed",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a845cc45bdef8ceb7d60",
+    "url": "/static/css/2.91bfcd09.chunk.css"
+  },
+  {
+    "revision": "6270d2e4d3e1631c9b0e",
+    "url": "/static/css/main.3100762d.chunk.css"
+  },
+  {
+    "revision": "a845cc45bdef8ceb7d60",
+    "url": "/static/js/2.d2ddd40d.chunk.js"
+  },
+  {
+    "revision": "535d749fb945e27b03a2d86da0c400c1",
+    "url": "/static/js/2.d2ddd40d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6270d2e4d3e1631c9b0e",
+    "url": "/static/js/main.31f27bc1.chunk.js"
+  },
+  {
+    "revision": "205362f430c2ef59ec204ce12ea80eea",
+    "url": "/static/js/my-first-worker.b02ed77c.chunk.worker.js"
+  },
+  {
+    "revision": "455f9f3ae849b1b7c9d5b5f2d351830a",
+    "url": "/static/js/my-first-worker.b02ed77c.chunk.worker.js.LICENSE"
+  },
+  {
+    "revision": "d159989b4357ecc5d7bc",
+    "url": "/static/js/runtime-main.308c05fc.js"
+  }
+]);
